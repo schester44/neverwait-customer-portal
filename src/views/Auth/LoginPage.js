20a7862b3241c36550c11d00eb78736ee3524985
Steven Chester <steven.chester@industryweapon.com>
2019-08-07 20:04:32 -0400

    fix login page

--- src/views/Auth/LoginPage.js
+++ src/views/Auth/LoginPage.js
@@ -20,18 +20,18 @@ const LoginPage = () => {
 	const handleSubmit = async () => {
 		await login({
 			variables: {
-                input: {
-                    contactNumber: fields.contactNumber,
-                    password: fields.password
-                }
+				input: {
+					contactNumber: fields.contactNumber,
+					password: fields.password
+				}
 			}
-        })
-        
+		})
+
 		window.location.reload()
 	}
 
 	return (
-		<div>
+		<div style={{ background: 'white', width: '100%', height: '100vh', padding: 10 }}>
 			<h1 style={{ textAlign: 'center', padding: 24 }}>NeverWait</h1>
 
 			<h2 style={{ padding: '0 0 10px 10px', fontWeight: 400 }}>Login</h2>

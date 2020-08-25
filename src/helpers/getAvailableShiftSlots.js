7d9c5a02b4763eeb9f9e30adec640fbc20b84f94
Steven Chester <steven.chester@industryweapon.com>
2020-08-25 19:38:27 -0400

    adjust slot logic to allow bookieng appointments at the same time as another appointment's end tim

--- src/helpers/getAvailableShiftSlots.js
+++ src/helpers/getAvailableShiftSlots.js
@@ -1,4 +1,4 @@
-import { isAfter, isWithinRange, addMinutes } from 'date-fns'
+import { isAfter, isWithinRange, addMinutes, subMinutes } from 'date-fns'
 import SchedulerCreator from './ScheduleCreator'
 
 const scheduler = new SchedulerCreator()
@@ -10,39 +10,45 @@ const getAvailableShiftSlots = (schedule, date, duration = 0, timeSlotInterval =
 
 	let shiftSlots = scheduler.getShiftSlots(workDay.schedule_shifts, timeSlotInterval, date)
 
-	const slots = shiftSlots.filter(slot => {
+	console.log(shiftSlots, schedule.appointments)
+	const slots = shiftSlots.filter((slot) => {
 		// Create slots based on the time this person works.
-		const isAvailable = [...schedule.appointments, ...schedule.blockedTimes].every(appointment => {
-			const slotStartOverlapsAppt = isWithinRange(
-				slot.start_time,
-				appointment.startTime,
-				appointment.endTime
-			)
+		const isAvailable = [...schedule.appointments, ...schedule.blockedTimes].every(
+			(appointment) => {
+				const slotStartOverlapsAppt = isWithinRange(
+					slot.start_time,
+					appointment.startTime,
+					// Subtracting 1 minute allows us to schedule an appointment at the same time that an appointment is ending. Example: an appointment runs from 8 to 8:30 -- this allows us to show an available slot for 8:30
+					subMinutes(appointment.endTime, 1)
+				)
 
-			const slotEndOverlapsAppt = isWithinRange(
-				slot.end_time,
-				appointment.startTime,
-				appointment.endTime
-			)
+				const slotEndOverlapsAppt = isWithinRange(
+					slot.end_time,
+					appointment.startTime,
+					subMinutes(appointment.endTime, 1)
+				)
 
-			const slotWithAddedDurationOverlapsAppointment = isWithinRange(
-				appointment.startTime,
-				slot.start_time,
-				addMinutes(slot.start_time, duration > 0 ? duration : 0)
-			)
+				// Once the duration is added to the slot time, is that new time during another appointment?
+				const slotWithAddedDurationOverlapsAppointment = isWithinRange(
+					appointment.startTime,
+					slot.start_time,
+					// Subtracting 1 minute allows us to schedule an appointment at the same time that an appointment is ending. Example: an appointment runs from 8 to 8:30 -- this allows us to show an available slot for 8:30
+					addMinutes(slot.start_time, duration > 0 ? duration : 0)
+				)
 
-			const appointmentExceedsSchedule = isAfter(
-				addMinutes(slot.start_time, duration > 0 ? duration : 0),
-				slot.shiftEnd
-			)
+				const appointmentExceedsSchedule = isAfter(
+					addMinutes(slot.start_time, duration > 0 ? duration : 0),
+					slot.shiftEnd
+				)
 
-			return (
-				!slotWithAddedDurationOverlapsAppointment &&
-				!slotStartOverlapsAppt &&
-				!slotEndOverlapsAppt &&
-				!appointmentExceedsSchedule
-			)
-		})
+				return (
+					!slotWithAddedDurationOverlapsAppointment &&
+					!slotStartOverlapsAppt &&
+					!slotEndOverlapsAppt &&
+					!appointmentExceedsSchedule
+				)
+			}
+		)
 
 		return isAvailable
 	})
